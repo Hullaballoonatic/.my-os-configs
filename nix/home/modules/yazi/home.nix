05239cb81a3035121b1744b51df2d9e50a6b76f4
Hullaballoonatic <hullaballoonatic@gmail.com>
2026-08-15 20:27:31 -0500

    configure yazi and starship

--- nix/home/modules/yazi/home.nix
+++ nix/home/modules/yazi/home.nix
@@ -3,5 +3,17 @@
 		enable = true;
 
 		enableNushellIntegration = true;
+
+		keymap = {
+			mgr.prepend_keymap = [
+				{
+					on = ["y"];
+					run = [
+						''shell -- for path in %s; do echo "file://$path"; done | wl-copy -t text/uri-list''
+						"yank"
+					];
+				}
+			];
+		};
 	};
 }
