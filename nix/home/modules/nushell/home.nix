c47265c032dddefcb51818483375be311fd1524c
CaseyStratton <casey.stratton@jbhunt.com>
2026-08-18 13:48:44 -0500

    source private env vars if file exists

--- nix/home/modules/nushell/home.nix
+++ nix/home/modules/nushell/home.nix
@@ -8,6 +8,10 @@
       alias manage = ^~/.my-os-configs/manage
     '';
 
+    extraEnv = ''
+      try { source-env ($env.HOME | path join ".secret_env.nu") }
+    '';
+
     settings.show_banner = false;
   };
 }
